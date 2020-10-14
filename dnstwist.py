87fbd12f32f2dc8ba1df0f1b37c9665c205d4d51
Justin <jmatt081@uottawa.ca>
2020-10-13 23:25:39 -0400

    Refactor

--- dnstwist.py
+++ dnstwist.py
@@ -2,35 +2,45 @@ import pandas
 from subprocess import run, PIPE
 from io import StringIO
 
+
 def dnstwist(whitelist):
     '''
-    Returns 2d numpy array of:
-        fuzzer, domain-name, dns-a, dns-aaaa, dns-mx, dns-ns, geoip-country, whois-created, ssdeep-score
+    Generates possible phishing domain names based on the
+    provided whitelist and verifies IP's, geolocations,
+    name service, site similarity to the original domain, etc.
+
+    Returns a DataFrame of:
+        fuzzer, domain-name, dns-a, dns-aaaa, dns-mx,
+        dns-ns, geoip-country, whois-created, ssdeep-score
     '''
     dnstwist_result_pandas = []
 
     for domain in whitelist:
         print(f'Generating possible domains for {domain}...')
         dnstwist_proc = run(
-            ['dnstwist',
-            '-r', '-w', '-g', '--ssdeep', '-m',
-            '-f', 'csv',
-            '--tld', './Dictionaries/english.dict',
-            '--tld', './Dictionaries/french.dict',
-            '--tld', './Dictionaries/common_tlds.dict',
-            domain],
+            [
+                'dnstwist',
+                '-r', '-w', '-g', '--ssdeep', '-m',
+                '-f', 'csv',
+                '--tld', './Dictionaries/english.dict',
+                '--tld', './Dictionaries/french.dict',
+                '--tld', './Dictionaries/common_tlds.dict',
+                domain
+            ],
             stdout=PIPE
-            )
+        )
 
         decoded_stdout = dnstwist_proc.stdout.decode()
-        
+
         stdout_data = StringIO(decoded_stdout)
 
         dnstwist_result_pandas.append(
             pandas.read_csv(stdout_data, sep=',')
         )
 
-    # Combine result pandas from each domain and remove duplicate entries if any exist.
-    unique_result_dataframe = pandas.concat(dnstwist_result_pandas).drop_duplicates().reset_index(drop=True)
+    # Combine pandas from each domain and remove duplicates.
+    unique_result_dataframe = pandas.concat(dnstwist_result_pandas)\
+                                    .drop_duplicates()\
+                                    .reset_index(drop=True)
 
     return unique_result_dataframe
--- httprobe.py
+++ httprobe.py
@@ -1,12 +1,19 @@
 from subprocess import run, Popen, PIPE
 import pandas
 
+
 def probe(domains):
+    """
+    Finds if a domain has http and/or https services running.
+
+    Returns a DataFrame of domain name, http service running,
+    and https service running.
+    """
     print('Probing domains for running http/https services...')
 
     input_domains = "\n".join(domains)
 
-    # cat temp file and pipe into httprobe    
+    # cat temp file and pipe into httprobe
     cat_proc = Popen(['echo', '-e', input_domains], stdout=PIPE)
     probe_output = run(['httprobe'], stdin=cat_proc.stdout, stdout=PIPE).stdout
     cat_proc.wait()
@@ -17,13 +24,15 @@ def probe(domains):
     if stdout_data[-1] == '':
         stdout_data.pop()
 
-    result_dataframe = pandas.DataFrame(columns=['domain-name', 'http-active', 'https-active'])
+    result_dataframe = pandas.DataFrame(
+        columns=['domain-name', 'http-active', 'https-active']
+    )
 
     for i, domain in enumerate(domains):
         result_dataframe.loc[i] = [
             domain,
-            int(f'http://{domain}' in stdout_data),
-            int(f'https://{domain}' in stdout_data)
-            ]
-    
+            f'http://{domain}' in stdout_data,
+            f'https://{domain}' in stdout_data
+        ]
+
     return result_dataframe
