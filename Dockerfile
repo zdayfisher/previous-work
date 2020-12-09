0a1525238b5f619e74d53a879360909cde6c4a0e
Justin <jmatt081@uottawa.ca>
2020-12-08 23:49:16 -0500

    Update Requirements
    Includes: moving dnstwist, DNSPython, whois module installation to requirements.txt and setup.py

--- Dockerfile
+++ Dockerfile
@@ -6,9 +6,6 @@ RUN apt-get update && apt-get install python3 python3-pip curl git -y
 # Copy project content to /opt folder
 #COPY . /opt/project...
 
-# Install DNSTwist
-RUN pip3 install dnstwist DNSPython whois
-
 # Install Golang
 RUN curl -O https://dl.google.com/go/go1.15.5.linux-amd64.tar.gz
 RUN tar xvf go1.15.5.linux-amd64.tar.gz && chown -R root:root ./go && mv go /usr/local && rm -rf go1.15.5.linux-amd64.tar.gz
--- setup.py
+++ setup.py
@@ -6,16 +6,21 @@ setup(
     packages=find_packages(),
     version='0.1.0',
     zip_safe=False,
-    #entry_points={
-    #    'console_scripts': [
-    #            'pkgdiscovery = pkg.discovery:main'
-    #        ]
-    #},
+    entry_points={
+        'console_scripts': [
+                'phishfinder = pkg.phishfinder:main'
+            ]
+    },
     install_requires=[
         'pandas',
         'crtsh',
         'dnstwist',
-        'tqdm'
+        'tqdm',
+        'numpy',
+        'tldextract',
+        'scikit-learn',
+        'DNSPython',
+        'whois'
     ],
     include_package_data=True
 )
